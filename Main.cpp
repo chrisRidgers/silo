8e72be29a48b9b9e0fd9197ba598eefc5c4cb078
RidgeDev <studentjester42@gmail.com>
2014-02-02 15:50:46 +0000

    just updating from local, no major

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <valarray>
 #include <vector>
 
 
@@ -14,10 +15,11 @@ enum{APP, SIZEX, SIZEY, INPUT, OUTPUT};
 using namespace std;
 
 int saveLandscape();
+int setupVerts(valarray<float>& ls);
 
-int sizeX = 1024, sizeY = 1024;
+int sizeX = 256, sizeY = 256;
 string pathOut = "output/";
-vector<float> landscape;
+valarray<float> landscape;
 
 int main(int argc, const char* argv[])
 {
@@ -40,12 +42,15 @@ int main(int argc, const char* argv[])
     << now->tm_sec
     << ".obj";
   pathOut += ss.str();
+  landscape.resize(sizeX*sizeY*3);
 
   cout << sizeX << endl;
   cout << sizeY << endl;
   cout << landscape.size() << endl;
   cout << pathOut << endl;
 
+  setupVerts(landscape);
+ // for(int i=0;i<landscape.size();i++) cout << landscape[i] << endl;
   saveLandscape();
 
   return 0;
@@ -56,6 +61,43 @@ int saveLandscape()
   ofstream output;
   output.open(pathOut);
   output << "Hello File World!" << endl;
+  for(int i=0; i<landscape.size(); i+=3)
+  {
+    valarray<float> vert = landscape[slice(i,3,1)];
+    output << "v\t" << vert[0] << "\t" << vert[1] << "\t" << vert[2] << endl;
+  }
   output.close();
+  return 0;
+}
+
+int setupVerts(valarray<float>& ls)
+{
+  int countX = 0;
+  int countY = 0;
+
+  for(int i=0; i<landscape.size(); i+=3)
+  {
+    //cout << countX << endl;
+    landscape[i]=countX;
+    if(countX >= 1023)
+    {
+      countX = 0;
+    }else
+    {
+      countX++;
+    }
+    //countX = countX>=1023 ? 0 : countX++;
+  }
+                
+  for(int i=1; i<landscape.size(); i+=15)
+  {
+   valarray<float> tempY = ls[slice(i,5,3)];
+   cout << countY << endl;
+   tempY=countY;
+   countY++;
+  }
+  
+
+
   return 0;
 }
